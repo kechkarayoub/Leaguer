40126a6df2d36173bcaf6d8b7361d15c3d34e4bc
PC <kechkarayoub@gmail.com>
2025-01-03 08:12:56 +0100

    Add comments

--- backend/accounts/views.py
+++ backend/accounts/views.py
@@ -23,9 +23,13 @@ def send_verification_email(user, handle_end_email_error=False):
     Returns:
         tuple: A tuple containing the status code (int) and token data (tuple).
     """
+    # Generate user's token
     token_ = default_token_generator.make_token(user)
+    # Get current timestamp as str
     timestamp_str = str(datetime.datetime.now().timestamp())
+    # Generate final token that contains timestamp for expiration validation
     token = token_ + "_*_" + timestamp_str
+    # Encode user's id
     uid = urlsafe_base64_encode(force_bytes(user.pk))
 
     # Build the verification URL
@@ -33,8 +37,9 @@ def send_verification_email(user, handle_end_email_error=False):
 
     # Render the email content
     subject = _("Verify Your Email Address")
-    from_email = settings.DEFAULT_FROM_EMAIL
+    # Get emails common context
     context = get_email_base_context()
+    # Add custom context to verification's email
     context.update({
         "email_title": _("Verify Your Email Address"), "user": user, "verification_url": verification_url,
     })
@@ -45,9 +50,10 @@ def send_verification_email(user, handle_end_email_error=False):
     try:
         print("html_content")
         print(html_content)
+        # This is for testing send email's error from third party
         if handle_end_email_error:
             err = int("text")
-        email = EmailMultiAlternatives(subject, text_content, from_email, [user.email])
+        email = EmailMultiAlternatives(subject, text_content, context.get('from_email'), [user.email])
         email.attach_alternative(html_content, "text/html")
         email.send()
     except (
@@ -75,22 +81,30 @@ def verify_user_email(uid, token_, resend_verification_email=False):
             (True if not verified and expired, False otherwise),
         ).
     """
+    # Get user's token and timestamp str from the token in request
     token_date = token_.split("_*_")
     token = token_date[0]
+    # Convert timestamp str to float if exists
     timestamp = len(token_date) == 2 and float(token_date[1])
+    # Get date of token creation
     date_token = timestamp and datetime.datetime.fromtimestamp(timestamp)
+    # Decode the user's coded id
     uid = urlsafe_base64_decode(uid).decode()
     user = User.objects.get(pk=uid)
 
+    # The email is already validated
     if user.is_email_validated:
         return True, True, False
+    # The resend_verification_email is True or the token is expired (not the same day)
     elif resend_verification_email or date_token and date_token.strftime("%Y-%m-%d") != datetime.datetime.now().strftime("%Y-%m-%d"):
         send_verification_email(user)
         return False, False, True
+    # If the token is valid, the email address will be validated
     if default_token_generator.check_token(user, token):
         user.is_email_validated = True
         user.save()
         return True, False, False
+    # If the token is not valid, the email address will not be validated
     else:
         return False, False, False
 
@@ -111,6 +125,7 @@ def verify_email(request):
 
     uid_ = urlsafe_base64_decode(uid).decode()
     user = User.objects.get(pk=uid_)
+    # Activate user's current language for translations
     activate(user.current_language)
     try:
         verified, already_verified, expired_token = verify_user_email(uid, token, resend_verification_email=resend_verification_email)
--- backend/leaguer/utils.py
+++ backend/leaguer/utils.py
@@ -16,5 +16,6 @@ def get_email_base_context(selected_language="fr"):
         'app_name': settings.APPLICATION_NAME,
         'current_year': datetime.datetime.now().year,
         'direction': "rtl" if selected_language == "ar" else "ltr",
+        'from_email': settings.DEFAULT_FROM_EMAIL,
     }
     return context
