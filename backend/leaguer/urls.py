e849435f1a61663a0f59d37fac8fa21e380853e2
PC <kechkarayoub@gmail.com>
2025-02-07 08:51:15 +0100

    add media url configuration

--- backend/leaguer/urls.py
+++ backend/leaguer/urls.py
@@ -14,14 +14,16 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
+from django.conf import settings
 from django.conf.urls.i18n import i18n_patterns
+from django.conf.urls.static import static
 from django.contrib import admin
 from django.urls import include, path, re_path
 
 urlpatterns = [
     path('accounts/', include('accounts.urls')),
     path('i18n/', include('django.conf.urls.i18n')),
-]
+] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
 
 urlpatterns += i18n_patterns(
     re_path(r'^admin/', admin.site.urls),
