a00144644dc3ba5e1eaaed40cf9b4678bcbe3cf8
PC <kechkarayoub@gmail.com>
2025-07-20 17:05:29 +0100

    debug daphne server start error

--- backend/leaguer/asgi.py
+++ backend/leaguer/asgi.py
@@ -10,17 +10,23 @@ https://docs.djangoproject.com/en/5.1/howto/deployment/asgi/
 from channels.auth import AuthMiddlewareStack
 from channels.routing import ProtocolTypeRouter, URLRouter
 from django.core.asgi import get_asgi_application
-import leaguer.ws_routing
 import os
 
 
 # Set the default Django settings module for the 'asgi' application
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'leaguer.settings')
 
+
+# ✅ Initialize Django before importing any routing/consumers
+django_asgi_app = get_asgi_application()
+
+
+import leaguer.ws_routing
+
 # Define the ASGI application with support for both HTTP and WebSocket protocols
 application = ProtocolTypeRouter({
     # Route traditional HTTP requests to Django's ASGI application
-    "http": get_asgi_application(),
+    "http": django_asgi_app,
     # Route WebSocket requests to the Channels AuthMiddleware and URLRouter
     "websocket": AuthMiddlewareStack(
         URLRouter(
